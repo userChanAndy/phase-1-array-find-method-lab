8a32c348bc4222c2064cfe3f36ffa5d3e006c46a
Andy Chan <andy.chan9930@gmail.com>
2021-10-15 12:42:55 -0400

    commit tree 80dc424cfb79f15a0f252a6ff77ef7bed9707951

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+const superbowlWin = array => {
+    let newArr = array.find(array => array.result === "W") 
+    if(newArr){
+        return newArr.year
+    } else {
+        return undefined
+    }
+}
+
+
+
 
